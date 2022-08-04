9bf7f85d58edb676723f74a91f1af53de590ac9d
Jacob Moseley <doomskull@hotmail.co.uk>
2022-08-04 18:40:23 +0100

    Completed "Plus Minus"

--- Plus Minus/main.cpp
+++ Plus Minus/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <iomanip>
+
+void plusMinus(const std::vector<int>& arr)
+{
+	double positives{};
+	double negatives{};
+	double zeros{};
+
+	for (const auto& num : arr)
+	{
+		if (num > 0) ++positives;
+
+		else if (num < 0) ++negatives;
+
+		else ++zeros;
+	}
+
+	std::cout << std::fixed << std::setprecision(6);
+
+	std::cout << positives / arr.size() << std::endl;
+	std::cout << negatives / arr.size() << std::endl;
+	std::cout << zeros / arr.size() << std::endl;
+}
 
 int main(int argc, char** argv)
 {
+
+	plusMinus(std::vector<int>{ -4, 3, -9, 0, 4, 1 });
 
 	return 0;
 }
