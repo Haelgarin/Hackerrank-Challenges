fe632d1b07795d2c8b07f2a6b0d2050e4e0ff994
Jacob Moseley <doomskull@hotmail.co.uk>
2022-08-05 20:00:44 +0100

    Minor formatting change

--- Time Conversion/main.cpp
+++ Time Conversion/main.cpp
@@ -4,35 +4,35 @@
 
 std::string timeConversion(const std::string& s)
 {
-    std::string postfix = s.substr(s.length() - 2, s.length() - 1); // Get the AM/PM postfix
-    std::string sMinusPostfixAndHour = s.substr(2, s.length() - 4);
-    std::string hourStr = s.substr(0, 2);
-    int hour;
+	std::string postfix = s.substr(s.length() - 2, s.length() - 1); // Get the AM/PM postfix
+	std::string sMinusPostfixAndHour = s.substr(2, s.length() - 4);
+	std::string hourStr = s.substr(0, 2);
+	int hour;
 
-    std::stringstream ss;
-    ss << hourStr;
-    ss >> hour;
+	std::stringstream ss;
+	ss << hourStr;
+	ss >> hour;
 
-    if ((hour == 12) && (postfix == "AM"))
-    {
-        hour -= 12;
-    }
+	if ((hour == 12) && (postfix == "AM"))
+	{
+		hour -= 12;
+	}
 
-    else if (((hour >= 1) && (hour < 12)) && postfix == "PM")
-    {
-        hour += 12;
-    }
+	else if (((hour >= 1) && (hour < 12)) && postfix == "PM")
+	{
+		hour += 12;
+	}
 
-    hourStr = "";
+	hourStr = "";
 
-    if (hour < 10)
-    {
-        hourStr.append("0");
-    }
+	if (hour < 10)
+	{
+		hourStr.append("0");
+	}
 
-    hourStr += std::to_string(hour);
+	hourStr += std::to_string(hour);
 
-    return hourStr + sMinusPostfixAndHour;
+	return hourStr + sMinusPostfixAndHour;
 }
 
 int main(int argc, char** argv)
