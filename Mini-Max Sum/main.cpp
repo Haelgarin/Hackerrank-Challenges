56dd56cf14c1d2fbe3acb9b46a4b5d8ea4e58aae
Jacob Moseley <doomskull@hotmail.co.uk>
2022-08-05 19:53:49 +0100

    Completed "Mini-Max Sum"

--- Mini-Max Sum/main.cpp
+++ Mini-Max Sum/main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <cstdint>
 
-void miniMaxSum(const std::vector<int>& arr)
+void miniMaxSum(std::vector<int> arr)
 {
+	std::sort(arr.begin(), arr.end());
 
+	int64_t minSum{};
+	int64_t maxSum{};
+
+	for (int64_t i{ 0 }; i < 4; ++i)
+	{
+		minSum += arr[i];
+		maxSum += arr[((arr.size() - 1) - i)];
+	}
+
+	std::cout << minSum << " " << maxSum << std::endl;
 }
 
 int main(int argc, char** argv)
 {
+
+	std::vector<int> vec{ 256741038, 623958417, 467905213, 714532089, 938071625 };
+
+	miniMaxSum(vec);
 
 	return 0;
 }
