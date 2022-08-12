8bfaf89c7473ddf774dd4e5968c9ea693a6d5d2c
Jacob Moseley <doomskull@hotmail.co.uk>
2022-08-12 15:32:17 +0100

    Created and completed "Subarray Division 2"

---
+++ Subarray Division 2/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+
+int birthday(const std::vector<int>& s, int d, int m) 
+{
+	int count{};
+
+	for (int i{ 0 }; i <= (s.size() - m); ++i)
+	{
+		int tempSum{};
+
+		for (int j{ 0 }; j < m; ++j)
+		{
+			tempSum += s[static_cast<int64_t>(i + j)];
+		}
+
+		if (tempSum == d)
+		{
+			++count;
+		}
+
+		tempSum = 0;
+	}
+
+	return count;
+}
+
+int main(int argc, char** argv)
+{
+
+	std::cout << birthday(std::vector<int>{ 2, 2, 1, 3, 2 }, 4, 2) << std::endl;
+
+	return 0;
+}
